9e6d8bc970557d932c54d64bbb2ba2cb0cb52393
pro100denysko <pro100denysko@gmail.com>
2018-04-03 20:20:47 +0300

    Update sql

--- src/main/resources/sql/data-cards.sql
+++ src/main/resources/sql/data-cards.sql
@@ -1,5 +1,9 @@
 INSERT INTO cards VALUES (1, 'card1', '1234567890123456', '123', 'male', 'address', 12000.0);
 INSERT INTO cards VALUES (2, 'card2', '1000200030004000', '123', 'male', 'address', 6000.0);
+INSERT INTO cards VALUES (3, 'card3', '9289432905108365', '123', 'male', 'address', 6000.0);
+INSERT INTO cards VALUES (4, 'card4', '6593002408792501', '123', 'male', 'address', 6000.0);
 
 INSERT INTO users VALUES (1, '123456', 1);
 INSERT INTO users VALUES (2, '123456', 2);
+INSERT INTO users VALUES (3, '123456', 3);
+INSERT INTO users VALUES (4, '123456', 4);
