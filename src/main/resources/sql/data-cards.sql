c0a5711ff8e2ed853d8b0be4f0854069a27b3689
pro100denysko <pro100denysko@gmail.com>
2018-04-03 19:25:17 +0300

    Update sql

--- src/main/resources/sql/data-cards.sql
+++ src/main/resources/sql/data-cards.sql
@@ -1,2 +1,5 @@
-INSERT INTO cards VALUES (1, 'userName', '1234567890123456', '123', 'male', 'address');
-INSERT INTO users VALUES (1, '123456', 1)
+INSERT INTO cards VALUES (1, 'card1', '1234567890123456', '123', 'male', 'address', 12000.0);
+INSERT INTO cards VALUES (2, 'card2', '1000200030004000', '123', 'male', 'address', 6000.0);
+
+INSERT INTO users VALUES (1, '123456', 1);
+INSERT INTO users VALUES (2, '123456', 2);
--- src/main/resources/sql/schema-cards.sql
+++ src/main/resources/sql/schema-cards.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS cards;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS transfer;
 
 -- Table: cards
 CREATE TABLE cards (
@@ -8,7 +9,9 @@ CREATE TABLE cards (
   number_of_card VARCHAR(255) NOT NULL,
   birth_date     VARCHAR(255) NOT NULL,
   sex            VARCHAR(255) NOT NULL,
-  address        VARCHAR(255) NOT NULL
+  address        VARCHAR(255) NOT NULL,
+  balance        DOUBLE       NOT NULL,
+  UNIQUE (number_of_card)
 );
 
 -- Table: users
@@ -16,4 +19,13 @@ CREATE TABLE users (
   id       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
   password VARCHAR(255) NOT NULL,
   card     VARCHAR(255) NOT NULL
+);
+
+-- Table: transfer
+CREATE TABLE transfer (
+  id                  INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  numberOfSendersCard VARCHAR(255) NOT NULL,
+  senders             VARCHAR(255) NOT NULL,
+  recipients_card     VARCHAR(255) NOT NULL,
+  amount              DOUBLE       NOT NULL
 )
