422f147d7c651bb2808c8665c19655f72c5cb886
pro100denysko <pro100denysko@gmail.com>
2018-04-03 19:24:45 +0300

    Add transfer method, rename urls, refactor

--- src/main/java/ua/com/app/controller/CardController.java
+++ src/main/java/ua/com/app/controller/CardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import ua.com.app.model.BankCard;
+import ua.com.app.model.Transfer;
 import ua.com.app.model.User;
 import ua.com.app.service.CardServiceImpl;
 
@@ -22,9 +23,9 @@ public class CardController {
     this.cardService = cardService;
   }
 
-  @RequestMapping(value = "/create-card", method = RequestMethod.POST)
+  @RequestMapping(value = "/cards", method = RequestMethod.POST)
   public ResponseEntity<String> createBankCard(@RequestBody BankCard card) {
-    BankCard newCard = cardService.save(card);
+    BankCard newCard = cardService.saveNewCard(card);
     if (newCard != null) {
       return ResponseEntity.status(HttpStatus.CREATED).build();
     } else {
@@ -37,6 +38,29 @@ public class CardController {
     BankCard newCard = cardService.findByNumber(user.getNumberOfCard());
     if (newCard != null && newCard.getUser().getPassword().equals(user.getPassword())) {
       return ResponseEntity.status(HttpStatus.OK).build();
+    } else {
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+  }
+
+  @RequestMapping(value = "/transfer", method = RequestMethod.POST)
+  public ResponseEntity<?> transfer(@RequestBody Transfer transfer) {
+    BankCard sendersCard = cardService.findByNumber(transfer.getSender().getNumberOfCard());
+    BankCard recipientsCard = cardService.findByNumber(transfer.getNumberOfRecipientsCard());
+    if (sendersCard != null && recipientsCard != null && sendersCard.getUser().getPassword()
+        .equals(transfer.getSender().getPassword())) {
+      if (sendersCard.getBalance() - transfer.getAmount() > 0) {
+        sendersCard.setBalance(sendersCard.getBalance() - transfer.getAmount());
+        recipientsCard.setBalance(recipientsCard.getBalance() + transfer.getAmount());
+        cardService.save(sendersCard);
+        cardService.save(recipientsCard);
+
+        //This fill is null because the user data enters the loop
+        sendersCard.getUser().setCard(null);
+        return ResponseEntity.status(HttpStatus.OK).body(sendersCard);
+      } else {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+      }
     } else {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
